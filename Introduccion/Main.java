25b2480a8a1efc30668bb1e54623c6a51f857f9c
Your Name <youexample.com>
2026-04-24 16:18:33 -0600

    Clases y objetos

---
+++ Introduccion/Main.java
@@ -0,0 +1,12 @@
+package Introduccion;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Perro miMascota = new Perro(); 
+        miMascota.nombre = "Terry";
+        miMascota.saludar();
+    }
+    
+}
---
+++ Introduccion/Perro.java
@@ -0,0 +1,10 @@
+package Introduccion;
+
+public class Perro {
+    String nombre;
+    int edad;
+
+    void saludar() {
+        System.out.println(nombre + " dice: ¡Guau!");
+    }
+}
